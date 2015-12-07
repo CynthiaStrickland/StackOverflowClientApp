4bcadb6eb582eb5db6960ece28d77edad1a1ba55
Notsosof1 <cawhitlatch2@gmail.com>
2015-12-07 09:47:03 -0800

    Initial Commit

---
+++ StackOverFlowClientApp/MyQuestionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyQuestionsViewController.h
+//  StackOverFlowClientApp
+//
+//  Created by Cynthia Whitlatch on 12/7/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyQuestionsViewController : UIViewController
+
+@end
---
+++ StackOverFlowClientApp/SearchQuestionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchQuestionsViewController.h
+//  StackOverFlowClientApp
+//
+//  Created by Cynthia Whitlatch on 12/7/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchQuestionsViewController : UIViewController
+
+@end
