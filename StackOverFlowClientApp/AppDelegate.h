bb51a065bc2616fbfc48a536ddbda708a43ab3fd
Notsosof1 <cawhitlatch2@gmail.com>
2015-12-07 09:21:52 -0800

    Initial Commit

---
+++ StackOverFlowClientApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackOverFlowClientApp
+//
+//  Created by Cynthia Whitlatch on 12/7/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackOverFlowClientApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackOverFlowClientApp
+//
+//  Created by Cynthia Whitlatch on 12/7/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
